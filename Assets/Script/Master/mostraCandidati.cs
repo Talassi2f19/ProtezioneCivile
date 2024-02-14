f25909f9be553d89ab34e3b2f1b520e6750c5791
pitty-eau <134259316+pitty-eau@users.noreply.github.com>
2024-02-14 10:13:21 +0100

    aggiornamento folder script

--- Assets/Script/Master/mostraCandidati.cs
+++ Assets/Script/Master/mostraCandidati.cs
@@ -21,15 +21,13 @@ public class mostraCandidati : MonoBehaviour
     
     private void AddCandidato(string str)
     {
-        Debug.Log("AddCandidato: stringValue =" + str);
-
         //gestisco il messaggio di keep alive periodico che arriva
         //così quel messaggio non passa per quello che viene dopo l'if
         if (str.Contains("event: patch"))
         {
-            List<string> nomi = new List<string>();
+            string nome = "";
             
-            //Contine il valore del campo "data" che viene ricevuto con il Listener
+            //Contine l'oggetto contenente il nome del candidato
             string dataJSON = str.Split("\"data\":")[1].Split("}")[0] + "}";
             
             Debug.Log("AddCandidato: stringAfterSplit =" + dataJSON);
@@ -37,18 +35,13 @@ public class mostraCandidati : MonoBehaviour
             //Trasformo il contenuto in un oggetto JSON in modo da ottenerne le chiavi
             JSONObject listaJSON = new JSONObject(dataJSON);
             
-            //Lista dei player che si sono candidati
-            nomi = listaJSON.keys;
-
-            foreach (string nome in nomi)
-            {
-                //Aggiunta prefab di testo alla lista
-                candidati.Add(GameObject.Instantiate(genericTextPrefab, contenitore));
-                //Inserisco il nome nel prefab del neocandidato
-                candidati[candidati.Count - 1].GetComponent<genericTextPrefab>().setGenericText(nome);
-                //Posizionamento oggetti
-                candidati[candidati.Count - 1].transform.position = new Vector3(100, 100 * (candidati.Count - 1), 0);
-            }
+            //Nome del player che si è candidato
+            nome = listaJSON.keys[0];
+            
+            //Aggiunta prefab di testo alla lista
+            candidati.Add(GameObject.Instantiate(genericTextPrefab, contenitore));
+            //Inserisco il nome nel prefab del neocandidato
+            candidati[candidati.Count - 1].GetComponent<genericTextPrefab>().setGenericText(nome);
         }
         
     }
