28424af1f0948774fdd156001ae395da76fd6bb8
sebaverri <134058981+sebaverri@users.noreply.github.com>
2024-02-14 16:25:26 +0100

    Update SelezioneCoC.cs

--- Assets/Script/userScript/SelezioneCoC.cs
+++ Assets/Script/userScript/SelezioneCoC.cs
@@ -8,12 +8,18 @@ using UnityEngine.SceneManagement;
 
 public class SelezioneCoC : MonoBehaviour
 {
+    [SerializeField] private GameObject mostraRuoloPrefab;
+    [SerializeField] private Transform mostraRuoloPrefabParent;
 
     [SerializeField] private GameObject buttonPrefab;
-    [SerializeField] private Transform parent;
+    [SerializeField] private Transform buttonParent;
     private Dictionary<string, JSONObject> player = new Dictionary<string, JSONObject>();
     void Start()
     {
+
+        GameObject mostraRuolo =  GameObject.Instantiate(mostraRuoloPrefab, mostraRuoloPrefabParent);
+        //TODO passare le variabili
+
         RestClient.Get(Info.DBUrl + Info.SessionCode + "/players.json").Then(e =>
         {
             JSONObject json = new JSONObject(e.Text);
@@ -28,8 +34,9 @@ public class SelezioneCoC : MonoBehaviour
 
         foreach(var pl in player)
         {
-           GameObject hh =  GameObject.Instantiate(buttonPrefab, parent);
-           
+           GameObject hh =  GameObject.Instantiate(buttonPrefab, buttonParent);
+           //TODO passare le variabili da passare
+           //pl.Key -> nome player
         }
     }
 
@@ -38,5 +45,8 @@ public class SelezioneCoC : MonoBehaviour
     {
         SceneManager.LoadScene("_Scenes/user/game");
     }
+
+
+
 
 }
