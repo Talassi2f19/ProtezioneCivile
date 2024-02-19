d00ea3b7bc5f6fae26ea5489ecdb718ed5916858
sebaverri <134058981+sebaverri@users.noreply.github.com>
2024-02-19 17:23:25 +0100

    Iniziato sviluppo gestione eventi
    Creazione scriptable object per memorizzare i dati dell'evento
    realizzata la struttura gerarchica delle evento tramite RestClient

---
+++ Assets/Script/User/GestioneMissioni.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using Proyecto26;
+using Script.Utility;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class GestioneMissioni : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /*
+    Ogni volta che viene lanciata una missione, viene creata una cartella nel database contenente tutte le informazioni
+    al riguardo
+    */
+    public void LanciaMissione(MissionScriptableObject mso)
+    {
+        string missionURL = mso.getMissionURL(); // URL/CODE/Missione/Code
+        
+        //Inserimento nella cartella della missione del suo nome
+        string nomeMissione = "{\"NomeMissione\":\"" + mso.nomeMissione + "\"}";
+        RestClient.Patch(missionURL + ".json", nomeMissione).Then(afterNome =>
+        {
+            //per ognuno degli step viene caricato il loro stato IsEnded e la lista di ruoli coinvolti per step
+            for (int i = 0; i < mso.numeroStep; i++)
+            {
+                //stato dello step
+                string statusFase = "{\"isEnded\":false}";
+                string[] ruoliStep = mso.ruoliPerStep[i].Split(",");
+                RestClient.Patch(missionURL + "/fasi/" + i + ".json", statusFase).Then(afterStatus =>
+                {
+                    for (int j = 0; j < mso.numeroStep; j++)
+                    {
+                        string patchRuolo = "{\"" + ruoliStep[j] + "\":false}";
+                        RestClient.Patch(missionURL + "/fasi/" + i + "/ruoli.json", patchRuolo);
+                    }
+                });
+            }
+        });
+        
+        
+    }
+    
+    
+}
---
+++ Assets/Script/Utility/MissionScriptableObject.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections.Generic;
+using Script.Utility;
+
+[CreateAssetMenu(fileName = "Missione", menuName = "ScriptableObjects/MissionScriptableObject", order = 1)]
+public class MissionScriptableObject : ScriptableObject
+{
+    public int codiceMissione;
+    public string nomeMissione = "Telecomunicazioni";
+    public int numeroStep;
+    public List<string> ruoliPerStep;
+
+    
+
+    public string getMissionURL()
+    {
+        return Info.DBUrl + Info.sessionCode + "/missioni/" + codiceMissione;
+    }
+    
+}
