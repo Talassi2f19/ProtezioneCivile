a11013746e8e73273a1d79a0c212133b62393570
pitty-eau <134259316+pitty-eau@users.noreply.github.com>
2024-02-21 14:53:04 +0100

    Update GestioneEventiPlayer.cs

--- Assets/Script/Utility/GestioneEventi/GestioneEventiPlayer.cs
+++ Assets/Script/Utility/GestioneEventi/GestioneEventiPlayer.cs
@@ -1,80 +1,81 @@
-using System.Collections;
 using System.Collections.Generic;
 using Defective.JSON;
-using Script.Utility;
 using UnityEngine;
 
-public class GestioneEventiPlayer : MonoBehaviour
+namespace Script.Utility.GestioneEventi
 {
-    private Listeners ascoltoEventi = new Listeners(Info.DBUrl + "AAAA" + "/Missioni.json");
-    private Dictionary<string, Missione> missioni = new Dictionary<string, Missione>();
-    
-    void Start()
+    public class GestioneEventiPlayer : MonoBehaviour
     {
-        ascoltoEventi.Start(GestisciMissione);
-    }
-
-
-    private void GestisciMissione(string str)
-    {   
-        Debug.Log(str);
-        if (!str.Contains("null"))
+        private Listeners ascoltoEventi = new Listeners(Info.DBUrl + "AAAA" + "/Missioni.json");
+        private Dictionary<string, Missione> missioni = new Dictionary<string, Missione>();
+    
+        void Start()
         {
-            if (str.Contains("\"path\":\"/\""))
+            ascoltoEventi.Start(GestisciMissione);
+        }
+
+
+        private void GestisciMissione(string str)
+        {   
+            Debug.Log(str);
+            if (!str.Contains("null"))
             {
-                //nuovo evento
-                // event: put
-                // data: {"path":"/","data":{"kkk":{"Fasi":{"fase1":{"Ruoli":{"Coc":false,"Sindaco":false},"Status":true},"fase2":{"Ruoli":{"sindaco":false},"Status":false}},"Nome":"nome"}}}
-                str = str.Split("\"data\":")[1];
-                
-                Dictionary<string, Missione> tmp = new JSONObject(str).ToMissioneDictionary();
-                foreach (var var in tmp)
+                if (str.Contains("\"path\":\"/\""))
                 {
-                    missioni.Add(var.Key, var.Value);
+                    //nuovo evento
+                    // event: put
+                    // data: {"path":"/","data":{"kkk":{"Fasi":{"fase1":{"Ruoli":{"Coc":false,"Sindaco":false},"Status":true},"fase2":{"Ruoli":{"sindaco":false},"Status":false}},"Nome":"nome"}}}
+                    str = str.Split("\"data\":")[1];
+                
+                    Dictionary<string, Missione> tmp = new JSONObject(str).ToMissioneDictionary();
+                    foreach (var var in tmp)
+                    {
+                        missioni.Add(var.Key, var.Value);
+                    }
                 }
-            }
-            else
-            {
-                //modifica di un qualche stato
-                // event: put
-                // data: {"path":"/kkk/Fasi/fase1/isCompleted","data":false}
-                // data: {"path":"/kkk/Fasi/fase1/Ruolo/Sindaco","data":false}
+                else
+                {
+                    //modifica di un qualche stato
+                    // event: put
+                    // data: {"path":"/kkk/Fasi/fase1/isCompleted","data":false}
+                    // data: {"path":"/kkk/Fasi/fase1/Ruolo/Sindaco","data":false}
                     //           0 1    2    3     4     5
-                //Valore di "data"
-                string data = str.Split("\"data\":")[1].Split("}")[0];
-                string path = str.Split("\"path\":\"")[1].Split("\",\"")[0];
-                string[] arr = path.Split("/");
+                    //Valore di "data"
+                    string data = str.Split("\"data\":")[1].Split("}")[0];
+                    string path = str.Split("\"path\":\"")[1].Split("\",\"")[0];
+                    string[] arr = path.Split("/");
                 
                 
-                //if str.contains("isCompleted")" 
+                    //if str.contains("isCompleted")" 
                     //missioni[n1].elencofasi[n3].iscomnpleterd = !
-                //else if (str.contains("Ruolo"))
+                    //else if (str.contains("Ruolo"))
                     //missioni[n1].elencofasi[n3].ruoli[n5] = !
                 
-                if (path.Contains("Codice/Fasi/CodiceFase/Ruoli/"))
-                {
+                    if (path.Contains("Codice/Fasi/CodiceFase/Ruoli/"))
+                    {
                     
-                    string ruoloSoggetto = arr[arr.Length - 1];
+                        string ruoloSoggetto = arr[arr.Length - 1];
                     
-                }
+                    }
                 
-                //Distinguere diversi casi:
-                /*
+                    //Distinguere diversi casi:
+                    /*
                  * viene modificato isCompleted
                  * viene modificato lo stato di un ruolo
                  */
 
-                //Se è IsCompleted fa una cosa
-                //Se si trova dentro Codice/Fasi/FaseX/Ruoli fa un altra roba
-            }
+                    //Se è IsCompleted fa una cosa
+                    //Se si trova dentro Codice/Fasi/FaseX/Ruoli fa un altra roba
+                }
             
            
 
            
+            }
         }
-    }
 
     
     
     
+    }
 }
