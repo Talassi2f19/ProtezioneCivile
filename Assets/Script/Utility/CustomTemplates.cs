3c27cdbb5985ec9600bdaac88cdfc5900953219f
sebaverri <134058981+sebaverri@users.noreply.github.com>
2024-02-21 12:58:22 +0100

    eventi update

--- Assets/Script/Utility/CustomTemplates.cs
+++ Assets/Script/Utility/CustomTemplates.cs
@@ -47,20 +47,20 @@ namespace Script.Utility
 
         public static Missione toMissione(this JSONObject jsonObject)
         {
+            
             string code = jsonObject.keys[0];
             var data = jsonObject.list[0];
             string nome = data["Nome"].stringValue;
             var fasi = data["Fasi"];
             
             Dictionary<string, Fase> di = new Dictionary<string, Fase>();
-            var lk = jsonObject.keys;
-            var lv = jsonObject.list;
+            var lk = fasi.keys;
+            var lv = fasi.list;
             for (int i = 0; i < lk.Count; i++)
             {
                 di.Add(lk[i], lv[i].ToFase());
                 di[lk[i]].setCodice(lk[i]);
             }
-            
             return new Missione(nome, code, di);
         }
 
--- Assets/Script/Utility/GestioneEventi/GestioneEventiPlayer.cs
+++ Assets/Script/Utility/GestioneEventi/GestioneEventiPlayer.cs
@@ -1,14 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Defective.JSON;
 using Script.Utility;
 using UnityEngine;
 
 public class GestioneEventiPlayer : MonoBehaviour
 {
     private Listeners ascoltoEventi = new Listeners(Info.DBUrl + "AAAA" + "/Missioni.json");
-     
+
     
-    // Start is called before the first frame update
     void Start()
     {
         ascoltoEventi.Start(GestisciMissione);
@@ -20,15 +20,33 @@ public class GestioneEventiPlayer : MonoBehaviour
         Debug.Log(str);
         if (!str.Contains("null"))
         {
+            if (str.Contains("\"path\":\"/\""))
+            {
+                //nuovo evento
+                // event: put
+                // data: {"path":"/","data":{"kkk":{"Fasi":{"fase1":{"Ruoli":{"Coc":false,"Sindaco":false},"Status":true},"fase2":{"Ruoli":{"sindaco":false},"Status":false}},"Nome":"nome"}}}
+                str = str.Split("\"data\":")[1];
+                
+                JSONObject json = new JSONObject(str);
+                Missione miss = json.toMissione();
+                Debug.Log(miss.printData());
+            }
+            else
+            {
+                //modifica di un qualche stato
+                // event: put
+                // data: {"path":"/kkk/Fasi/fase1/Status","data":false}
+                
+                
+                
+            }
             
+           
+
+           
         }
     }
-    
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
     
     
     
