537ba0c40ad3b59facf5fe40c57d5a2f40719ef3
sebaverri <134058981+sebaverri@users.noreply.github.com>
2024-02-22 14:12:01 +0100

    Update AttesaRuoli.cs

--- Assets/Script/User/AttesaRuoli.cs
+++ Assets/Script/User/AttesaRuoli.cs
@@ -1,5 +1,6 @@
 using System;
 using Script.Utility;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using Scene = Script.Utility.Scene;
@@ -21,7 +22,7 @@ namespace Script.User
         private void TrovaRuolo(string str)
         {
             Debug.Log(str);
-            if (!str.Contains("null"))
+            if (!str.ContainsInsensitive("null"))
             {
                 listeners.Stop();
                 str = str.Split("\"data\":\"")[1].Split("\"")[0];
