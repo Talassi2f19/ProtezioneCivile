b1e47dc8d03c3472df04933e1808ea58ab1567ab
pitty-eau <134259316+pitty-eau@users.noreply.github.com>
2024-02-23 15:47:30 +0100

    Update PlayerOnline.cs

--- Assets/Script/User/Prefabs/PlayerOnline.cs
+++ Assets/Script/User/Prefabs/PlayerOnline.cs
@@ -2,6 +2,7 @@ using Script.Utility;
 using TMPro;
 using UnityEngine;
 using System;
+using Unity.VisualScripting;
 
 // ReSharper disable CommentTypo IdentifierTypo StringLiteralTypo
 namespace Script.User.Prefabs
@@ -11,7 +12,7 @@ namespace Script.User.Prefabs
     {
         [SerializeField] private GameObject nome;
         public float speed = 1;
-        private Rigidbody2D rb;
+        private Rigidbody2D playerOnlineHitbox;
         private GenericUser genericUser;
         private Vector2 posizione = Vector2.zero;
         private Animator anim;
@@ -19,10 +20,10 @@ namespace Script.User.Prefabs
         private void Start()
         {
             anim = gameObject.GetComponent<Animator>();
-            rb = GetComponent<Rigidbody2D>();
+            playerOnlineHitbox = GetComponent<Rigidbody2D>();
             //sr.color = new Color(Random.value,Random.value,Random.value);
             gameObject.name = genericUser.name;
-            rb.position = posizione = genericUser.coord;
+            playerOnlineHitbox.position = posizione = genericUser.coord;
         }
     
         public void SetUser(GenericUser user)
@@ -34,27 +35,39 @@ namespace Script.User.Prefabs
 
         public void Move(Vector2 v)
         {
-            v.x = v.x!=0 ? v.x : rb.position.x ;
-            v.y = v.y!=0 ? v.y : rb.position.y ;
+            v.x = v.x!=0 ? v.x : playerOnlineHitbox.position.x ;
+            v.y = v.y!=0 ? v.y : playerOnlineHitbox.position.y ;
             posizione = v;
         }
 
         private void FixedUpdate()
         {
-            if (rb.position != posizione)
+            double dist;
+            if (playerOnlineHitbox.position != posizione)
             {
+                dist = GetDistance();
+                
                 transform.position = Vector2.MoveTowards(transform.position, posizione, speed * Time.deltaTime);
             }
+            else
+                dist = 0;
+            Animazione((float)dist);
+        }
+
+        private double GetDistance()
+        {
+            return Math.Sqrt(Math.Pow(playerOnlineHitbox.position.x - posizione.x, 2) + Math.Pow(playerOnlineHitbox.position.y - posizione.y, 2));
         }
         
-        private void Animazione()
+        private void Animazione(float distance)
         {
+
             if (posizione.x == 0 && posizione.y == 0)
                 anim.SetBool("IsStill", true);
             else
                 anim.SetBool("IsStill", false);
             
-            if (posizione.x > Math.Sqrt(2) / 2) //destra
+            if (posizione.x > (Math.Sqrt(2) / 2 * distance)) //destra
                 anim.SetBool("IsRight", true);
             else
                 anim.SetBool("IsRight", false);
@@ -64,12 +77,12 @@ namespace Script.User.Prefabs
             else
                 anim.SetBool("IsLeft", false);
         
-            if (posizione.y > Math.Sqrt(2) / 2 && (posizione.x >= - Math.Sqrt(2) / 2 && posizione.x <= Math.Sqrt(2) / 2)) //avanti
+            if (posizione.y > Math.Sqrt(2) / 2 && (posizione.x >= (- Math.Sqrt(2) / 2 * distance) && posizione.x <= (Math.Sqrt(2) / 2 * distance))) //avanti
                 anim.SetBool("IsUp", true);
             else
                 anim.SetBool("IsUp", false);
         
-            if (posizione.y < - Math.Sqrt(2) / 2 && (posizione.x >= - Math.Sqrt(2) / 2 && posizione.x <= Math.Sqrt(2) / 2)) //indietro
+            if (posizione.y < - Math.Sqrt(2) / 2 && (posizione.x >= (- Math.Sqrt(2) / 2 * distance) && posizione.x <= (Math.Sqrt(2) / 2 * distance))) //indietro
                 anim.SetBool("IsDown", true);
             else
                 anim.SetBool("IsDown", false);
