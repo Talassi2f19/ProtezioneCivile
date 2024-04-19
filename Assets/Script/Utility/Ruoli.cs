4c52c15138812310d0ea7d100e94664f4eef77fa
sebaverri <134058981+sebaverri@users.noreply.github.com>
2024-04-19 19:49:24 +0200

    aggiunti volontari

--- Assets/Script/Utility/Ruoli.cs
+++ Assets/Script/Utility/Ruoli.cs
@@ -7,6 +7,20 @@ namespace Script.Utility
     {
         Null,
         Sindaco,
-        Coc
+        Coc,
+        RefCri, //referente Croce Rossa Italiana
+        VolCri, //volontario Croce Rossa Italiana
+        Medico,
+        RefPC, //referente Protezione Civile
+        VolPC, //volontario Protezione Civile
+        Giornalista,
+        Tlc, //telecomunicazioni
+        RefPolizia, //referente Polizia
+        Polizia, //"volontario" polizia 
+        RefFuoco, //referente vigile del Fuoco
+        VolFuoco, //volontario Vigile del Fuovo
+        Segretaria,
+        RefGgev, // referente Guardie Giurate Ecologiche Volontarie
+        VolGgev //volontario Guardie Giurate Ecologiche Volontarie
     }
 }
