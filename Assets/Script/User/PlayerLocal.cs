65c0f2309c04c44422c1f00a777ee25f54c3c990
sebaverri <134058981+sebaverri@users.noreply.github.com>
2024-04-26 22:55:44 +0200

    collisioni

--- Assets/Script/User/PlayerLocal.cs
+++ Assets/Script/User/PlayerLocal.cs
@@ -23,12 +23,18 @@ namespace Script.User
         private Rigidbody2D rb;
 
         private Vector2 lastPosition;
+        private static readonly int IsStill = Animator.StringToHash("IsStill");
+        private static readonly int IsRight = Animator.StringToHash("IsRight");
+        private static readonly int IsLeft = Animator.StringToHash("IsLeft");
+        private static readonly int IsUp = Animator.StringToHash("IsUp");
+        private static readonly int IsDown = Animator.StringToHash("IsDown");
 
         private void Start()
         {
             anim = gameObject.GetComponent<Animator>();
             rb = GetComponent<Rigidbody2D>();
             lastPosition = rb.position;
+            castCollisions = new List<RaycastHit2D>();
         }
 
         private void FixedUpdate()
@@ -36,38 +42,50 @@ namespace Script.User
             //se ci sono movimenti in input
             if (movementInput != Vector2.zero)
             {
-                //collisioni 
-                //TODO da controllare il funzionamento delle collisioni 
-                // int count = rb.Cast(
-                //     movementInput,
-                //     movementFilter,
-                //     castCollisions,
-                //     moveSpeed * Time.fixedDeltaTime * collisionOffset);
-
-                //se non ci sono state collisioni sposta il player
-                // if (count == 0)
-                {
-                    //muove il player
-                    rb.MovePosition(rb.position + movementInput * (moveSpeed * Time.fixedDeltaTime));
-                    
-                    //animazione
-                    Animazione();
-                    
-                    
-
-                    //se la distanza lo consente carica sul server la posizione
-                    if (Vector2.Distance(lastPosition, rb.position) >= distanzaInvio)
-                    {
-                        string toSend = JsonUtility.ToJson(rb.position);
-                        RestClient.Patch(Info.DBUrl + Info.sessionCode + "/" + Global.PlayerFolder + "/" + Info.localUser.name + "/" + Global.CoordPlayerKey + ".json", toSend);
-                        lastPosition = rb.position;
-                    }
-                }
+                //aggiorna animazione
+                Animazione();
+                bool success = TryMove(movementInput);
+                // if (!success)
+                // {
+                //     success = TryMove(new Vector2(movementInput.x, 0));
+                //     if (!success)
+                //     {
+                //         success = TryMove(new Vector2(0, movementInput.y));
+                //     }
+                // }
             }
             else
             {
                 Animazione();
             }
+        }
+
+        private bool TryMove(Vector2 direction)
+        {
+            //collisioni 
+            int count = rb.Cast(
+                direction,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime * collisionOffset);
+            // se non ci sono state collisioni sposta il player
+            if (count == 0)
+            {
+                //muove il player
+                rb.MovePosition(rb.position + direction * (moveSpeed * Time.fixedDeltaTime));
+                
+                //se la distanza lo consente carica sul server la posizione
+                if (Vector2.Distance(lastPosition, rb.position) >= distanzaInvio)
+                {
+                    string toSend = JsonUtility.ToJson(rb.position);
+                    RestClient.Patch(
+                        Info.DBUrl + Info.sessionCode + "/" + Global.PlayerFolder + "/" + Info.localUser.name + "/" +
+                        Global.CoordPlayerKey + ".json", toSend);
+                    lastPosition = rb.position;
+                }
+                return true;
+            }
+            return false;
         }
 
         //funzione che viene richiamata da input system
@@ -82,38 +100,33 @@ namespace Script.User
             movementInput = moveValue;
         }
         
-        
-        //BUG Il personaggio si muove in modo strano quando fai impazzire il joystick
-        //Si consiglia un fix
-
         private void Animazione()
         {
             if (movementInput.x == 0 && movementInput.y == 0)
-                anim.SetBool("IsStill", true);
+                anim.SetBool(IsStill, true);
             else
-                anim.SetBool("IsStill", false);
+                anim.SetBool(IsStill, false);
             
             if (movementInput.x > Math.Sqrt(2) / 2) //destra
-                anim.SetBool("IsRight", true);
+                anim.SetBool(IsRight, true);
             else
-                anim.SetBool("IsRight", false);
+                anim.SetBool(IsRight, false);
         
             if (movementInput.x < - Math.Sqrt(2) / 2) //sinistra
-                anim.SetBool("IsLeft", true);
+                anim.SetBool(IsLeft, true);
             else
-                anim.SetBool("IsLeft", false);
+                anim.SetBool(IsLeft, false);
         
             if (movementInput.y > Math.Sqrt(2) / 2 && (movementInput.x >= - Math.Sqrt(2) / 2 && movementInput.x <= Math.Sqrt(2) / 2)) //avanti
-                anim.SetBool("IsUp", true);
+                anim.SetBool(IsUp, true);
             else
-                anim.SetBool("IsUp", false);
+                anim.SetBool(IsUp, false);
         
             if (movementInput.y < - Math.Sqrt(2) / 2 && (movementInput.x >= - Math.Sqrt(2) / 2 && movementInput.x <= Math.Sqrt(2) / 2)) //indietro
-                anim.SetBool("IsDown", true);
+                anim.SetBool(IsDown, true);
             else
-                anim.SetBool("IsDown", false);
+                anim.SetBool(IsDown, false);
 
-      //      Debug.Log(movementInput.x + " - " + movementInput.y);
         }
     }
 }
