dceb1a0e1013d8b166d6ee74475e81677ac8f78b
sebaverri <134058981+sebaverri@users.noreply.github.com>
2024-05-13 19:26:09 +0200

    fix listeners

--- Assets/Script/Utility/FirebaseEventsHandler.cs
+++ Assets/Script/Utility/FirebaseEventsHandler.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Text;
+using UnityEngine;
 using UnityEngine.Networking;
 
 // ReSharper disable CommentTypo
@@ -9,23 +11,41 @@ namespace Script.Utility
     {
         public delegate void DataReceivedEvents(string data);
         public event DataReceivedEvents DataReceived;
-        private byte[] bytes;
+        // private byte[] bytes;
 
-        protected override byte[] GetData()
-        {
-            return bytes;
-        }
+        // protected override byte[] GetData()
+        // {
+        //     return bytes;
+        // }
 
         protected override bool ReceiveData(byte[] data, int dataLength)
         {
             if (data == null || data.Length < 1)
                 return false;
-
-            this.bytes = data;
+            // bytes = data;
             if (DataReceived != null)
-                DataReceived(Encoding.UTF8.GetString(data));
-            PrintListener.Print(Encoding.UTF8.GetString(data));
+            {
+                String encodedData = Encoding.UTF8.GetString(data);
+                foreach (var tmp in encodedData.TrimEnd('\n').Split("\n\n"))
+                {
+                    DataReceived(tmp);
+                    Print(tmp);
+                }
+            }
             return true;
+        }
+        
+        
+        
+        private static int counter = 0;
+        private void Print(String text)
+        {
+            String kk = "<color=brown>" + counter++ + "</color>";
+            foreach (String tmp in text.Split("\n"))
+            {
+                kk += "<color=orange>" + tmp + "</color>\n";
+            }
+            Debug.LogWarning(kk);
         }
     }
 }
