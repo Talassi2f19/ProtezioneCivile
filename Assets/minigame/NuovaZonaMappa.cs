5ad264f100c23c2e25d70b9aaabe0e6885b089de
sebaverri <134058981+sebaverri@users.noreply.github.com>
2024-07-09 18:31:56 +0200

    Update NuovaZonaMappa.cs

--- Assets/minigame/NuovaZonaMappa.cs
+++ Assets/minigame/NuovaZonaMappa.cs
@@ -5,23 +5,23 @@ namespace minigame
 {
     public class NuovaZonaMappa : MonoBehaviour
     {
-        private BoxCollider2D collider;
+        private BoxCollider2D colliders;
         // Start is called before the first frame update
         void Start()
         {
-            collider = GetComponent<BoxCollider2D>();
+            colliders = GetComponent<BoxCollider2D>();
             StartCoroutine(ExecuteWithDelay());
         }
     
         private void OnTriggerStay2D(Collider2D other)
         {
             other.transform.position = new Vector3(-6.9f, 13f, 0);
-            collider.enabled = false;
+            colliders.enabled = false;
         }
     
         IEnumerator ExecuteWithDelay() {
             yield return new WaitForSeconds(1f);
-            collider.enabled = false;
+            colliders.enabled = false;
         }
     
     }
